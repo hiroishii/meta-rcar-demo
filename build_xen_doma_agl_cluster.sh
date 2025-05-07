8b1972436a6a9dbefe5dca694023b9d50589854d
Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>
2025-05-07 11:41:10 +0900

    build: yocto: Fix image generation error introduced by rm_work
    There were several errors introduced by rm_work, since some of the
    tasks are depended on the files located in work directories.
    As a temporary workaround for it, we'd apply RM_WORK_EXCLUDE
    for the producers of such files.
    
    Signed-off-by: Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>

--- build_xen_doma_agl_cluster.sh
+++ build_xen_doma_agl_cluster.sh
@@ -83,7 +83,18 @@ moulin prod-devel-rcar-virtio.yaml \
     --GRAPHICS binaries \
 
 # workaround: Modify build.ninja to inherit rm_work, since moulin seems not to be able to handle "+=" syntax
-sed -i -e "s/conf\ =\ /conf\ =\ \'INHERIT\ \+\=\ \"rm_work\"\'\ /g" build.ninja
+# Also, we'd apply RM_WORK_EXCLUDE for some of the tasks to avoid build error caused by referreing to the intermediate files in the work directories
+sed -i -e "s/conf\ =\ /conf\ =\ \
+    \'INHERIT\ \+\=\ \"rm_work\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" xen\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" xen-tools\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" u-boot\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" arm-trusted-firmware\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" linux-renesas\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" linux-generic-armv8\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" core-image-weston\"\'\ \
+    \'RM_WORK_EXCLUDE\ \+\=\ \" core-image-thin-initramfs\"\'\ \
+    /g" build.ninja
 
 # Cleanup build directory
 rm -rf firmware
